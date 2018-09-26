623c04eec878ca6f87db7b75fef5b8ada7ec2c08
Gootsu <40689389+Gootsu@users.noreply.github.com>
2018-09-26 19:40:54 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 
 /**
-@license
-Copyright Gootsu. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that can be
-found in the LICENSE file at https://github.com/Gootsu/test/blob/master/LICENSE 
-*/
+ * @license
+ * Copyright Gootsu. All Rights Reserved.
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/Gootsu/test/blob/master/LICENSE 
+ */
